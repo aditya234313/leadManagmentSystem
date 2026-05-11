a8f9585cf5b4f83f6710c7edaa81a242ab696f84
Aditya Sharma <your-github-email@gmail.com>
2026-05-11 11:21:24 +0530

    Final UI update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -25,25 +25,25 @@ function App() {
 
   return (
     <div
-    style={{
-      maxWidth: "1000px",
-      margin: "40px auto",
-      padding: "30px",
-      fontFamily: "Arial",
-      background: "#f5f5f5",
-      borderRadius: "10px",
-      boxShadow: "0 0 10px rgba(0,0,0,0.1)",
-  }}
+      style={{
+        maxWidth: "1000px",
+        margin: "40px auto",
+        padding: "30px",
+        fontFamily: "Arial",
+        background: "#f5f5f5",
+        borderRadius: "10px",
+        boxShadow: "0 0 10px rgba(0,0,0,0.1)",
+      }}
     >
       <h1
-    style={{
-      textAlign: "center",
-      marginBottom: "30px",
-      color: "#333",
-  }}
->
-  Lead Management System
-</h1>
+        style={{
+          textAlign: "center",
+          marginBottom: "30px",
+          color: "#333",
+        }}
+      >
+        Lead Management System
+      </h1>
 
       <LeadForm fetchLeads={fetchLeads} />
 
